4eb394d6ba1e385793191362205a6256e4190313
Pat <graczyk.pat@gmail.com>
2018-09-04 20:30:06 +0100

    mvp

---
+++ models/park.js
@@ -0,0 +1,58 @@
+const Park = function(name, price){
+  this.name = name;
+  this.price =price;
+  this.dinosaurs = [];
+}
+
+Park.prototype.numberOfDinosaurs = function(){
+  return this.dinosaurs.length;
+}
+
+Park.prototype.addDinosaurs = function(dinosaur){
+  this.dinosaurs.push(dinosaur);
+}
+
+Park.prototype.removeDinosaurs = function(dinosaur){
+  this.dinosaurs.pop(dinosaur);
+}
+
+Park.prototype.mostVisitedDinosaurOne = function(){
+   return Math.max(this.dinosaurs.guestsAttractedPerDay);
+}
+
+Park.prototype.mostVisitedDinosaur = function(){
+  let popularDinosour = this.dinosaurs[0];
+  for (let dinosaur of this.dinosaurs){
+    if (dinosaur.guestsAttractedPerDay > popularDinosour.guestsAttractedPerDay) {
+      popularDinosour = dinosaur;
+    }
+  }
+  return popularDinosour.species;
+}
+
+Park.prototype.mostVisitedDinosaurOne = function(){
+   return Math.max(this.dinosaurs.guestsAttractedPerDay);
+}
+
+Park.prototype.allDinosaurOfSpecies = function(species){
+  let allDinosOfKind = [];
+  for (let dinosaur of this.dinosaurs){
+    if (dinosaur.species === species){
+        allDinosOfKind.push(dinosaur)
+    }
+  }
+    return allDinosOfKind;
+}
+
+Park.prototype.removeDinosaursOfKind = function(species){
+  let dinosaoursLeft = [];
+  for (const dinosaur of this.dinosaurs){
+    if (dinosaur.species !== species){
+        dinosaoursLeft.push(dinosaur)
+    }
+  }
+  this.dinosaurs = dinosaoursLeft;
+}
+
+
+module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -3,25 +3,71 @@ const Park = require('../models/park.js');
 const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
+  let park;
+  let dinosaur1;
+  let dinosaur2;
+
 
   beforeEach(function () {
+    park = new Park("Central Park", 50);
+    dinosaur1 = new Dinosaur("t-rex", "herbivore", 20);
+    dinosaur2 = new Dinosaur("t-notrex", "herbivore", 50);
+    dinosaur3 = new Dinosaur("ttt-rex", "carnivore", 30);
+  });
 
-  })
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, "Central Park");
+  });
 
-  it('should have a name');
+  it('should have a ticket price', function(){
+    const actual = park.price;
+    assert.strictEqual(actual, 50);
+  });
 
-  it('should have a ticket price');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual,[]);
+  });
 
-  it('should have a collection of dinosaurs');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.addDinosaurs(dinosaur1);
+    const actual = park.numberOfDinosaurs();
+    assert.strictEqual(actual, 1);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.addDinosaurs(dinosaur1);
+    park.addDinosaurs(dinosaur2);
+    park.removeDinosaurs(dinosaur1);
+    const actual = park.numberOfDinosaurs();
+    assert.strictEqual(actual, 1);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    park.addDinosaurs(dinosaur1);
+    park.addDinosaurs(dinosaur2);
+    const actual = park.mostVisitedDinosaur();
+    assert.strictEqual(actual, 't-notrex');
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find all dinosaurs of a particular species', function(){
+    park.addDinosaurs(dinosaur1);
+    park.addDinosaurs(dinosaur2);
+    park.addDinosaurs(dinosaur3);
+    const actual = park.allDinosaurOfSpecies("herbivore");
+    assert.deepStrictEqual(actual, []);
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to remove all dinosaurs of a particular species', function(){
+    park.addDinosaurs(dinosaur1);
+    park.addDinosaurs(dinosaur2);
+    park.addDinosaurs(dinosaur3);
+    park.removeDinosaursOfKind("carnivore");
+    const actual = park.numberOfDinosaurs();
+    assert.strictEqual(actual, 3);
+  });
 
-  it('should be able to remove all dinosaurs of a particular species');
+  
 
 });
